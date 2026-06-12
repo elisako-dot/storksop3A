34b87b441893909a73824ff9f5f5b6f10485e056
elisako-dot <elisa.ko@stork11.com>
2026-06-12 18:06:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: "./",
+    base: '/storksop3A/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
