26c0cabc44b34901e1eb155a067b361e8b7b91bf
elisako-dot <elisa.ko@stork11.com>
2026-06-12 15:04:50 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/storksop3A/', 
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
